3daead606197ececff97836e189ca2d270f5c656
RawNotHUB <kungaklll@gmail.com>
2025-03-23 23:43:45 +0700

    Create Not.lua

---
+++ Not.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [116495829188952] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
+  [70876832253163] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
+}
+
+return Games
